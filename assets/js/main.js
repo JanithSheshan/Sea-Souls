014a7432d2a30b30260671b8f49de7653c0d669b
JaniShesh <jasheshan011@gmail.com>
2024-03-29 16:08:28 +0530

    services

--- assets/js/main.js
+++ assets/js/main.js
@@ -271,3 +271,23 @@ var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
     },
   }
 });
+
+function myFunction1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+
+function myFunction4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
